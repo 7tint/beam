bd045f9d483fe174263046ada9517b6b4bbafd1d
kwong <kirkwong33@gmail.com>
2021-03-06 14:52:10 -0800

    Update feed_card.js

--- components/feed_card.js
+++ components/feed_card.js
@@ -7,12 +7,12 @@ export const FeedCard = (props) => {
   return (    
     <Card>
         <View style={styles.cardContainer}>
-            <View style={{flex: 0.7}}>
+            <View style={styles.textContainer}>
                 <Text>Share with {props.friend}</Text>
                 <Text>{props.title}</Text>
                 <Text>Based on {props.context}</Text>
             </View>
-            <View>
+            <View style={styles.imageContainer}>
                 <Card.Image source={props.image_url} style={styles.imageSize}>
                 </Card.Image>
             </View>            
@@ -22,11 +22,21 @@ export const FeedCard = (props) => {
 }
 
 const styles = StyleSheet.create({
+    textContainer: {
+        flex: 0.8,
+    },  
     cardContainer: {
-        flexDirection: "row",
+        flexDirection: "row",        
     },
     imageSize: {
         width: 50,
-        height: 50
+        height: 50,
+        resizeMode: 'contain'
+    },
+    imageContainer: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 0.2
     }
 });
